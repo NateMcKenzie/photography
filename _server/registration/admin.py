99aca74b3ba90924ae2aea54e4b22a006bc650ec
Nate M <nate6264@gmail.com>
2023-12-01 11:21:53 -0700

    Update css for login. Activate admin page

--- _server/registration/admin.py
+++ _server/registration/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from core.views import ReservationRequest, ReservationConfirmed
 # Register your models here.
+admin.site.register(ReservationRequest)
+admin.site.register(ReservationConfirmed)
