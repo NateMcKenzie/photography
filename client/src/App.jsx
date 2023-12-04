2b77ddd18c80db0ec881d204878aeedc911aa3f7
Nate M <nate6264@gmail.com>
2023-12-03 22:11:16 -0700

    First pass on react portion redesign

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,6 @@ import './App.css'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 
-function Reservations() {
-  console.log("Reservations")
-}
-
-function Samples() {
-  console.log("Samples")
-}
-
 function App() {
 
   return (
